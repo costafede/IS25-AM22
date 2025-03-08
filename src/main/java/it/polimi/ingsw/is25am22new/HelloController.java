6858de91b7ede01c207a6ee850f63f7cb7cbfbe8
Federico Costa <federico.costa@alumni.rui.it>
2025-03-08 17:46:57 +0100

    Add Maven ecc files

---
+++ src/main/java/it/polimi/ingsw/is25am22new/HelloController.java
@@ -0,0 +1,14 @@
+package it.polimi.ingsw.is25am22new;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module it.polimi.ingsw.is25am22new {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens it.polimi.ingsw.is25am22new to javafx.fxml;
+    exports it.polimi.ingsw.is25am22new;
+}
