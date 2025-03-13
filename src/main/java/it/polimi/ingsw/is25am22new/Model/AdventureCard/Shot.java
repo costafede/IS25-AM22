b7c97704375c6d9f38d11dd14abd8511b46590c0
Tommaso Dong <dongtommaso03@gmail.com>
2025-03-13 22:31:43 +0100

    jhd: Updated Shot: Enum -> Class

--- src/main/java/it/polimi/ingsw/is25am22new/Model/AdventureCard/Shot.java
+++ src/main/java/it/polimi/ingsw/is25am22new/Model/AdventureCard/Shot.java
@@ -1,5 +1,15 @@
 package it.polimi.ingsw.is25am22new.Model.AdventureCard;
 
-public enum Shot {
-    BIG, SMALL;
+public class Shot {
+    private boolean big;
+    private Orientation orientation;
+
+    public Shot(boolean big, Orientation orientation) {
+        this.big = big;
+        this.orientation = orientation;
+    }
+
+    public boolean isBig() { return big; }
+
+    public Orientation getOrientation() { return orientation; }
 }
