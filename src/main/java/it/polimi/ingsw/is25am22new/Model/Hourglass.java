e0b85c4890b44b778c53214950e2069a4ec13b6a
Tommaso Dong <dongtommaso03@gmail.com>
2025-03-15 18:21:52 +0100

    JHD: Hourglass.java ADDED

---
+++ src/main/java/it/polimi/ingsw/is25am22new/Model/Hourglass.java
@@ -0,0 +1,44 @@
+package it.polimi.ingsw.is25am22new.Model;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Hourglass {
+    private Timer timer;
+    private int remainingSeconds;
+    boolean active;
+
+    public Hourglass(int remainingSeconds) {
+        this.timer = new Timer();
+        this.remainingSeconds = remainingSeconds;
+        this.active = false;
+    }
+
+    public void startTimer(Runnable callbackMethod) {
+        if (active) return; // So it doesn't start a new timer if one is already active
+
+        active = true;
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                if (remainingSeconds > 0) {
+                    remainingSeconds--;
+                } else {
+                    stopTimer();
+                    callbackMethod.run(); // Executes the callback (an action) when it finishes
+                }
+            }
+        }, 0, 1000);
+    }
+
+    public void stopTimer() {
+        if (active) {
+            timer.cancel();
+            active = false;
+        }
+    }
+
+    public int getRemainingSeconds() {
+        return remainingSeconds;
+    }
+}
