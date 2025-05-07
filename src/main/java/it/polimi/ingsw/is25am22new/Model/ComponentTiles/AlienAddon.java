8a04f8b7b04e32063410b353fdc68ea8c604822d
Toly <anatoly.contu@mail.polimi.it>
2025-05-07 14:45:56 +0200

    AC - [work in progress] TUI fixed language in componentTiles draw and fixed shieldGenerator draw

--- src/main/java/it/polimi/ingsw/is25am22new/Model/ComponentTiles/AlienAddon.java
+++ src/main/java/it/polimi/ingsw/is25am22new/Model/ComponentTiles/AlienAddon.java
@@ -29,22 +29,22 @@ public class AlienAddon extends ComponentTile {
         String right;
         String type = "";
 
-        if (topSide.equals(SMOOTH)){top = "L";}
+        if (topSide.equals(SMOOTH)){top = "S";}
         else if (topSide.equals(ONEPIPE)){top = "1";}
         else if (topSide.equals(TWOPIPES)){top = "2";}
         else {top = "3";}
 
-        if (bottomSide.equals(SMOOTH)){bottom = "L";}
+        if (bottomSide.equals(SMOOTH)){bottom = "S";}
         else if (bottomSide.equals(ONEPIPE)){bottom = "1";}
         else if (bottomSide.equals(TWOPIPES)){bottom = "2";}
         else {bottom = "3";}
 
-        if (leftSide.equals(SMOOTH)){left = "L";}
+        if (leftSide.equals(SMOOTH)){left = "S";}
         else if (leftSide.equals(ONEPIPE)){left = "1";}
         else if (leftSide.equals(TWOPIPES)){left = "2";}
         else {left = "3";}
 
-        if (rightSide.equals(SMOOTH)){right = "L";}
+        if (rightSide.equals(SMOOTH)){right = "S";}
         else if (rightSide.equals(ONEPIPE)){right = "1";}
         else if (rightSide.equals(TWOPIPES)){right = "2";}
         else {right = "3";}
--- src/main/java/it/polimi/ingsw/is25am22new/Model/ComponentTiles/DoubleEngine.java
+++ src/main/java/it/polimi/ingsw/is25am22new/Model/ComponentTiles/DoubleEngine.java
@@ -52,25 +52,25 @@ public class DoubleEngine extends Engine {
         String right;
         String active = "";
 
-        if (topSide.equals(SMOOTH) && !topSideEngine){top = "L";}
+        if (topSide.equals(SMOOTH) && !topSideEngine){top = "S";}
         else if (topSide.equals(ONEPIPE) && !topSideEngine){top = "1";}
         else if (topSide.equals(TWOPIPES) && !topSideEngine){top = "2";}
         else if (topSide.equals(UNIVERSALPIPE) && !topSideEngine){top = "3";}
         else {top = "#";}
 
-        if (bottomSide.equals(SMOOTH) && !bottomSideEngine){bottom = "L";}
+        if (bottomSide.equals(SMOOTH) && !bottomSideEngine){bottom = "S";}
         else if (bottomSide.equals(ONEPIPE) && !bottomSideEngine){bottom = "1";}
         else if (bottomSide.equals(TWOPIPES) && !bottomSideEngine){bottom = "2";}
         else if (bottomSide.equals(UNIVERSALPIPE) && !bottomSideEngine){bottom = "3";}
         else {bottom = "#";}
 
-        if (leftSide.equals(SMOOTH) && !leftSideEngine){left = "L";}
+        if (leftSide.equals(SMOOTH) && !leftSideEngine){left = "S";}
         else if (leftSide.equals(ONEPIPE) && !leftSideEngine){left = "1";}
         else if (leftSide.equals(TWOPIPES) && !leftSideEngine){left = "2";}
         else if (leftSide.equals(UNIVERSALPIPE) && !leftSideEngine){left = "3";}
         else {left = "#";}
 
-        if (rightSide.equals(SMOOTH) && !rightSideEngine){right = "L";}
+        if (rightSide.equals(SMOOTH) && !rightSideEngine){right = "S";}
         else if (rightSide.equals(ONEPIPE) && !rightSideEngine){right = "1";}
         else if (rightSide.equals(TWOPIPES) && !rightSideEngine){right = "2";}
         else if (rightSide.equals(UNIVERSALPIPE) && !rightSideEngine){right = "3";}
