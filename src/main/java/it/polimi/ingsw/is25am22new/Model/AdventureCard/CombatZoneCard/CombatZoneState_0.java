a01ef8f6de2ed728852e6098bea944873e1b8a7a
Tommaso Dong <dongtommaso03@gmail.com>
2025-05-10 17:19:58 +0200

    JHD: FIXED combatzone with only one player case

--- src/main/java/it/polimi/ingsw/is25am22new/Model/AdventureCard/CombatZoneCard/CombatZoneState_0.java
+++ src/main/java/it/polimi/ingsw/is25am22new/Model/AdventureCard/CombatZoneCard/CombatZoneState_0.java
@@ -11,23 +11,29 @@ public class CombatZoneState_0 extends CombatZoneState implements Serializable {
 
     @Override
     public void activateEffect(InputCommand inputCommand) {
-        // player with the fewest crew members loses daysOnFlight
-        String playerFewestMembers = game.getCurrPlayer();
-        int minCrewNumber = game.getShipboards().get(playerFewestMembers).getCrewNumber();
-        for(String player : game.getPlayerList()){
-            if(game.getShipboards().get(player).getCrewNumber() < minCrewNumber){
-                playerFewestMembers = player;
-            } else if (game.getShipboards().get(player).getCrewNumber() == minCrewNumber) {
-                playerFewestMembers =
-                        // who is ahead receives penalty
-                        game.getShipboards().get(player).getDaysOnFlight() >
-                                game.getShipboards().get(playerFewestMembers).getDaysOnFlight() ?
-                                player : playerFewestMembers;
+        if(game.getPlayerList().size() > 1) {
+            // player with the fewest crew members loses daysOnFlight
+            String playerFewestMembers = game.getCurrPlayer();
+            int minCrewNumber = game.getShipboards().get(playerFewestMembers).getCrewNumber();
+            for (String player : game.getPlayerList()) {
+                if (game.getShipboards().get(player).getCrewNumber() < minCrewNumber) {
+                    playerFewestMembers = player;
+                } else if (game.getShipboards().get(player).getCrewNumber() == minCrewNumber) {
+                    playerFewestMembers =
+                            // who is ahead receives penalty
+                            game.getShipboards().get(player).getDaysOnFlight() >
+                                    game.getShipboards().get(playerFewestMembers).getDaysOnFlight() ?
+                                    player : playerFewestMembers;
+                }
             }
+            game.getFlightboard().shiftRocket(playerFewestMembers, combatZoneCard.getFlightDaysLost());
+            game.setCurrPlayerToLeader();
+            transition(new CombatZoneState_1(combatZoneCard));
+        } else {
+            game.manageInvalidPlayers();
+            game.setCurrPlayerToLeader();
+            game.setCurrCard(null);
         }
-        game.getFlightboard().shiftRocket(playerFewestMembers, combatZoneCard.getFlightDaysLost());
-        game.setCurrPlayerToLeader();
-        transition(new CombatZoneState_1(combatZoneCard));
     }
 
     @Override
--- src/main/java/it/polimi/ingsw/is25am22new/Model/AdventureCard/CombatZoneCard2/CombatZoneState2_1.java
+++ src/main/java/it/polimi/ingsw/is25am22new/Model/AdventureCard/CombatZoneCard2/CombatZoneState2_1.java
@@ -15,62 +15,65 @@ public class CombatZoneState2_1 extends CombatZoneState2{
 
     @Override
     public void activateEffect(InputCommand inputCommand) {
-        String currentPlayer = game.getCurrPlayer();
-        Shipboard shipboard = game.getShipboards().get(currentPlayer);
+        if(game.getPlayerList().size() > 1) {
+            String currentPlayer = game.getCurrPlayer();
+            Shipboard shipboard = game.getShipboards().get(currentPlayer);
 
-        if(inputCommand.getChoice()) {// are you sure you want to use the battery?
-            int x = inputCommand.getRow();
-            int y = inputCommand.getCol();
-            AtomicInteger numOfBatteries = new AtomicInteger(0);
-            Optional<ComponentTile> ctOptional = shipboard.getComponentTileFromGrid(x, y);
-            // if player clicks on empty batteryComponent -> systems asks to confirm but cannot activate effect
-            // because the flag setBatteryUsed is still false
-            ctOptional.ifPresent(ct -> numOfBatteries.set(ct.getNumOfBatteries()));
-            if(numOfBatteries.get() > 0) {
-                ctOptional.ifPresent(ComponentTile::removeBatteryToken);
-                combatZoneCard2.setBatteryUsed(true);
-            }
-            transition(new CombatZoneState2_2(combatZoneCard2));
-        }
-        else {
-            if(game.getCurrPlayer().equals(game.getLastPlayer())) { // if last player
-                combatZoneCard2.getPlayerToStrength().put(currentPlayer, shipboard.getCannonStrength());
-                String playerLowestCannon = game.getCurrPlayer();
-                double lowestCannon = combatZoneCard2.getPlayerToStrength().get(playerLowestCannon);
-                for(String player : combatZoneCard2.getPlayerToStrength().keySet()) {
-                    if(combatZoneCard2.getPlayerToStrength().get(player) < lowestCannon) {
-                        playerLowestCannon = player;
-                        lowestCannon = combatZoneCard2.getPlayerToStrength().get(playerLowestCannon);
-                    } else if (combatZoneCard2.getPlayerToStrength().get(player) == lowestCannon) {
-                        playerLowestCannon =
-                                // who is ahead receives penalty
-                                game.getShipboards().get(player).getDaysOnFlight() >
-                                        game.getShipboards().get(playerLowestCannon).getDaysOnFlight() ?
-                                        player : playerLowestCannon;
-                    }
+            if (inputCommand.getChoice()) {// are you sure you want to use the battery?
+                int x = inputCommand.getRow();
+                int y = inputCommand.getCol();
+                AtomicInteger numOfBatteries = new AtomicInteger(0);
+                Optional<ComponentTile> ctOptional = shipboard.getComponentTileFromGrid(x, y);
+                // if player clicks on empty batteryComponent -> systems asks to confirm but cannot activate effect
+                // because the flag setBatteryUsed is still false
+                ctOptional.ifPresent(ct -> numOfBatteries.set(ct.getNumOfBatteries()));
+                if (numOfBatteries.get() > 0) {
+                    ctOptional.ifPresent(ComponentTile::removeBatteryToken);
+                    combatZoneCard2.setBatteryUsed(true);
                 }
-
-                // deactivates all components for all players
-                for(String player : combatZoneCard2.getPlayerToStrength().keySet()) {
-                    for(int i = 0; i < 5; i++){
-                        for(int j = 0; j < 7; j++){
-                            game.getShipboards().get(player).getComponentTileFromGrid(i ,j).ifPresent(ComponentTile::deactivateComponent);
+                transition(new CombatZoneState2_2(combatZoneCard2));
+            } else {
+                if (game.getCurrPlayer().equals(game.getLastPlayer())) { // if last player
+                    combatZoneCard2.getPlayerToStrength().put(currentPlayer, shipboard.getCannonStrength());
+                    String playerLowestCannon = game.getCurrPlayer();
+                    double lowestCannon = combatZoneCard2.getPlayerToStrength().get(playerLowestCannon);
+                    for (String player : combatZoneCard2.getPlayerToStrength().keySet()) {
+                        if (combatZoneCard2.getPlayerToStrength().get(player) < lowestCannon) {
+                            playerLowestCannon = player;
+                            lowestCannon = combatZoneCard2.getPlayerToStrength().get(playerLowestCannon);
+                        } else if (combatZoneCard2.getPlayerToStrength().get(player) == lowestCannon) {
+                            playerLowestCannon =
+                                    // who is ahead receives penalty
+                                    game.getShipboards().get(player).getDaysOnFlight() >
+                                            game.getShipboards().get(playerLowestCannon).getDaysOnFlight() ?
+                                            player : playerLowestCannon;
                         }
                     }
+
+                    // deactivates all components for all players
+                    for (String player : combatZoneCard2.getPlayerToStrength().keySet()) {
+                        for (int i = 0; i < 5; i++) {
+                            for (int j = 0; j < 7; j++) {
+                                game.getShipboards().get(player).getComponentTileFromGrid(i, j).ifPresent(ComponentTile::deactivateComponent);
+                            }
+                        }
+                    }
+
+                    game.getFlightboard().shiftRocket(playerLowestCannon, combatZoneCard2.getFlightDaysLost());
+                    game.setCurrPlayerToLeader();
+
+                    transition(new CombatZoneState2_3(combatZoneCard2));
+                } else { // if not last player
+                    combatZoneCard2.getPlayerToStrength().put(currentPlayer, shipboard.getCannonStrength());
+                    game.setCurrPlayerToNext();
+                    transition(new CombatZoneState2_1(combatZoneCard2));
                 }
-
-                game.getFlightboard().shiftRocket(playerLowestCannon, combatZoneCard2.getFlightDaysLost());
-                game.setCurrPlayerToLeader();
-
-                transition(new CombatZoneState2_3(combatZoneCard2));
             }
-            else { // if not last player
-                combatZoneCard2.getPlayerToStrength().put(currentPlayer, shipboard.getCannonStrength());
-                game.setCurrPlayerToNext();
-                transition(new CombatZoneState2_1(combatZoneCard2));
-            }
+        } else {
+            game.manageInvalidPlayers();
+            game.setCurrPlayerToLeader();
+            game.setCurrCard(null);
         }
-
     }
 
     @Override
