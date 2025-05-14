5ec6d03c28a6b8fdcd4012956cbc41f9776bdd0b
Toly <anatoly.contu@mail.polimi.it>
2025-05-14 23:05:38 +0200

    AC - showCard user experience fix

--- src/main/java/it/polimi/ingsw/is25am22new/Client/View/AdventureCardView.java
+++ src/main/java/it/polimi/ingsw/is25am22new/Client/View/AdventureCardView.java
@@ -52,7 +52,17 @@ public class AdventureCardView {
         for (int i = 0; i < card.getPlanets().size(); i++) {
             System.out.print("Planet" + i + ": ");
             if(card.getPlanets().get(i).playerPresent()){
-                System.out.println("A player is present. You can't land mate");
+                System.out.println("A player is present");
+                System.out.println();
+                Map<GoodBlock, Integer> theoreticalGoodblocks = card.getPlanets().get(i).getTheoreticalGoodblocks();
+                theoreticalGoodblocks.forEach((goodBlock, quantity) ->
+                        System.out.println(goodBlock + ", Quantity: " + quantity)
+                );
+                System.out.println();
+                Map<GoodBlock, Integer> actualGoodblocks = card.getPlanets().get(i).getActualGoodblocks();
+                actualGoodblocks.forEach((goodBlock, quantity) ->
+                        System.out.println(goodBlock + ", Available: " + quantity)
+                );
             }
             else{
                 System.out.println("No player is present. You can land, enjoy!");
