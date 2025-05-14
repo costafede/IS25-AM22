394e196159b76ef67e5fae41ecc834e636a66629
Tommaso Dong <dongtommaso03@gmail.com>
2025-05-14 14:02:46 +0200

    JHD: ADDED socket exit

--- src/main/java/it/polimi/ingsw/is25am22new/HelloApplication.java
+++ src/main/java/it/polimi/ingsw/is25am22new/HelloApplication.java
@@ -3,16 +3,20 @@ package it.polimi.ingsw.is25am22new;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("StartMenu.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
+        Image icon = new Image(Objects.requireNonNull(getClass().getResource("/it/polimi/ingsw/is25am22new/Graphics/Icon.png")).toString());
+        stage.getIcons().add(icon);
+        stage.setTitle("Galaxy Trucker");
         stage.setScene(scene);
         stage.show();
     }
