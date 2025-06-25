62bcbc943817660acbb771ac411561b367bf6804
EmanueleDeSimone04 <emanuele1.desimone@mail.polimi.it>
2025-06-25 22:49:03 +0200

    EDS: minor fixes

--- src/main/java/it/polimi/ingsw/is25am22new/Model/Persistence/GameLoader.java
+++
@@ -1,6 +0,0 @@
-package it.polimi.ingsw.is25am22new.Model.Persistence;
-
-
-public class GameLoader {
-
-}
--- src/main/java/it/polimi/ingsw/is25am22new/Model/Persistence/GameSaver.java
+++
@@ -1,5 +0,0 @@
-package it.polimi.ingsw.is25am22new.Model.Persistence;
-
-
-public class GameSaver {
-}
